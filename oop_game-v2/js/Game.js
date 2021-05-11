5dcd95eb5b6f72ae945ec598792b2c6977fe30bc
Ja'Quan Thompson <jaquanthompson87@icloud.com>
2021-05-10 21:20:51 -0400

    Commit
    Working on the next step of the project.

--- oop_game-v2/js/Game.js
+++ oop_game-v2/js/Game.js
@@ -2,10 +2,25 @@
  * Project 4 - OOP Game App
  * Game.js */
 
-// class Game () {
-//     constructor () {
-        missed = 0;
-        phrases = []; 
-        activePhrase = null;
-//     }
-// }
+class Game  {
+    constructor() {
+        this.missed = 0;
+        this.phrases = this.createPhrases(); 
+        this.activePhrase = null;
+};   
+
+/**
+* Creates phrases for use in game
+* @return {array} An array of phrases that could be used in the game
+*/
+
+createPhrases() {
+    const phraseObjects = [
+        new Phrase ('Life is like a box of chocolates'),
+        new Phrase ('Javascript is Amazing'),
+        new Phrase ('I love investing'),
+        new Phrase ('I will become a FullStack Developer'),
+        new Phrase ('Redbull gives you wings')
+    ];
+    return phraseObjects;
+}};
